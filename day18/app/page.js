4f14fa970055681946ef6853debce7f3aa54de65
Ajith Chakkadath <toolsticks@gmail.com>
2024-06-29 16:00:59 +0530

    day18

---
+++ day18/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div className="h-[700px] w-[400px] bg-red-100 bg-gradient-to-b from-[#080E1A] via-[#435876]  via-65% to-[#4A5F79] ">
+        <div></div>
+
+      </div>
+    </main>
+  );
+}
